0d17eaada1a77498b3b5a89f9a9857284f140bea
kb <kb@kb-pc>
2016-05-10 09:18:42 -0500

    Added the main classes

---
+++ extracredit.moviewreview/src/main/java/mum/edu/cs544/Controller/MainController.java
@@ -0,0 +1,10 @@
+package mum.edu.cs544.Controller;
+
+public class MainController {
+	
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
---
+++ extracredit.moviewreview/src/main/java/mum/edu/cs544/Controller/MovieController.java
@@ -0,0 +1,5 @@
+package mum.edu.cs544.Controller;
+
+public class MovieController {
+
+}
